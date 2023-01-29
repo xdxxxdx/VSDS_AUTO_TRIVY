3a925b04b7d227ea5885fc77b7dafdc38712c7b7
X D <xueying_deng@outlook.com>
2023-01-29 20:08:32 +0100

    Some update

--- trivyImage.sh
+++ trivyImage.sh
@@ -1,3 +1,18 @@
+
+
+LDES_WORKBENCH_NIFI_TAG=20230116t113945
+LDES_SERVER_TAG=20230112t1553
+LDES_CLIENT_CLI_TAG=20230116t1123
+
+JSON_DATA_GENERATOR_TAG=20230113t0736
+LDES_SERVER_SIMULATOR_TAG=20230113t0736
+LDES_CLIENT_SINK_TAG=20230113t0736
+
+GTFS2LDES_TAG=20220928t1555
+MONGODB_TAG=6.0.3
+
+
+
 #trivy the docker images
 echo "############trivy scan ghcr.io/informatievlaanderen/ldes-server############"
 trivy image --offline-scan  --security-checks vuln,secret,config --severity HIGH,CRITICAL ${DOCKER_REPO:-ghcr.io/informatievlaanderen}/ldes-server:${LDES_SERVER_TAG:-20230112t1553}> ./DockerImage/ldes-server.output;
--- trivyRepo.sh
+++ trivyRepo.sh
@@ -1,6 +1,10 @@
 #trivy the github repository with a specific commit
-#export BASH_ENV=user.env
-#export GITHUB_TOKEN=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;
+VSDS_LDESConnectors=5886a1461f2565cee0e2bda8ef8650129ba32da8
+VSDS_LDES_E2E_testing=f5a60040b20d277b1b239b25d2052773c298bed3
+VSDS_LDESServer4J=0fc4b2073bbc1abc26ec025a4beb2eca80fdf36c
+VSDS_LDESWorkbench_NiFi=efe3ecbe736bd6d787845bbcb71f857447eb2dd5
+VSDS_LDESWorkbench_Services=2891957dad109793d91525bdcf60dd73c842bd7f
+export GITHUB_TOKEN=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;
 echo "############trivy scan ../VSDS-LDESConnectors############"
 trivy repo --offline-scan  --security-checks vuln,secret,config --severity HIGH,CRITICAL --commit ${VSDS_LDESConnectors:-4996d3b82eb2a241154bbf7547f4cfe04f114af0} -o ./RRepoCom/VSDS-LDESConnectors.output  https://github.com/Informatievlaanderen/VSDS-LDESConnectors;
 echo "############trivy scan ../VSDS-LDESServer4############"
@@ -9,7 +13,7 @@ echo "############trivy scan ../VSDS-LDESWorkbench-NiFi############"
 trivy repo --offline-scan  --security-checks vuln,secret,config --severity HIGH,CRITICAL --commit ${VSDS_LDESWorkbench_NiFi:-4996d3b82eb2a241154bbf7547f4cfe04f114af0} -o ./RRepoCom//VSDS-LDESWorkbench-NiFi.output  https://github.com/Informatievlaanderen//VSDS-LDESWorkbench-NiFi;
 echo "############trivy scan ../VSDS-LDESWorkbench-Services############"
 trivy repo --offline-scan  --security-checks vuln,secret,config --severity HIGH,CRITICAL --commit ${VSDS_LDESWorkbench_Services:-4996d3b82eb2a241154bbf7547f4cfe04f114af0} -o ./RRepoCom/VSDS-LDESWorkbench-Services.output  https://github.com/Informatievlaanderen/VSDS-LDESWorkbench-Services;
-echo "############trivy scan ../VSDS-LDES-E2E-testin############" 
-trivy repo --offline-scan  --security-checks vuln,secret,config --severity HIGH,CRITICAL --commit ${VSDS_LDES_E2E_testing:-f5a60040b20d277b1b239b25d2052773c298bed3} -o ./RRepoCom/VSDS-LDES-E2E-testing.output  https://github.com/Informatievlaanderen/VSDS-LDES-E2E-testing;'
+echo "############trivy scan ../VSDS-LDES-E2E-testing############" 
+trivy repo --offline-scan  --security-checks vuln,secret,config --severity HIGH,CRITICAL --commit ${VSDS_LDES_E2E_testing:-f5a60040b2052773c298bed3} -o ./RRepoCom/VSDS-LDES-E2E-testing.output  https://github.com/Informatievlaanderen/VSDS-LDES-E2E-testing;
 
 
