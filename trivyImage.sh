84fb3db1cf181bac3fe13893afdd2413796a119f
X D <xueying_deng@outlook.com>
2023-01-30 13:40:00 +0100

    Finalization

--- trivyImage.sh
+++ trivyImage.sh
@@ -1,11 +1,12 @@
 
 #trivy the docker images: Runtime parameters, please modify them accordingly
+# No need to download the image to local 
 
-LDES_WORKBENCH_NIFI_TAG=20230116t113945
-LDES_SERVER_TAG=20230112t1553
-LDES_CLIENT_CLI_TAG=20230116t1123
+LDES_WORKBENCH_NIFI_TAG=main
+LDES_SERVER_TAG=main
+LDES_CLIENT_CLI_TAG=main
 
-JSON_DATA_GENERATOR_TAG=20230113t0736
+JSON_DATA_GENERATOR_TAG=main
 LDES_SERVER_SIMULATOR_TAG=20230113t0736
 LDES_CLIENT_SINK_TAG=20230113t0736
 
@@ -15,11 +16,11 @@ MONGODB_TAG=6.0.3
 
 
 #trivy the docker images
-echo "############trivy scan ghcr.io/informatievlaanderen/ldes-server############"
+echo -e "\n\n############trivy scan ghcr.io/informatievlaanderen/ldes-server############\n\n"
 trivy image --offline-scan  --security-checks vuln,secret,config --severity HIGH,CRITICAL ${DOCKER_REPO:-ghcr.io/informatievlaanderen}/ldes-server:${LDES_SERVER_TAG:-20230112t1553}> ./DockerImage/ldes-server.output;
-echo "############trivy scan ghcr.io/informatievlaanderen/ldes-cli, VSDS-LDESWorkbench-Services ############"
+echo -e "\n\n############trivy scan ghcr.io/informatievlaanderen/ldes-cli, VSDS-LDESWorkbench-Services ############\n\n"
 trivy image --offline-scan --security-checks vuln,secret,config --severity HIGH,CRITICAL ${DOCKER_REPO:-ghcr.io/informatievlaanderen}/ldes-cli:${LDES_CLIENT_CLI_TAG:-20230116t1123} >  ./DockerImage/ldes-cli-VSDS-LDESWorkbench-Services.output;
-echo "############trivy scan ghcr.io/informatievlaanderen/ldes-workbench-nifi############"
+echo -e "\n\n############trivy scan ghcr.io/informatievlaanderen/ldes-workbench-nifi############\n\n"
 trivy image --offline-scan --security-checks vuln,secret,config --severity HIGH,CRITICAL  ${DOCKER_REPO:-ghcr.io/informatievlaanderen}/ldes-workbench-nifi:${LDES_WORKBENCH_NIFI_TAG:-20230116t113945} >  ./DockerImage/ldes-workbench-nifi.output;
-echo "############rivy scan ghcr.io/informatievlaanderen/json-data-generator############" 
-trivy image --offline-scan --security-checks vuln,secret,config --severity HIGH,CRITICAL ${DOCKER_REPO:-ghcr.io/informatievlaanderen}/json-data-generator:${JSON_DATA_GENERATOR_TAG:-20230113t0736} >  ./DockerImage/json-data-generator.output;
+'echo "############trivy scan ghcr.io/informatievlaanderen/json-data-generator############" 
+trivy image --offline-scan --security-checks vuln,secret,config --severity HIGH,CRITICAL ${DOCKER_REPO:-ghcr.io/informatievlaanderen}/json-data-generator:${JSON_DATA_GENERATOR_TAG:-20230113t0736} >  ./DockerImage/json-data-generator.output;'
--- trivyRepo.sh
+++ trivyRepo.sh
@@ -1,6 +1,6 @@
 #trivy the github repository with a specific commit:Runtime parameters, please modify them accordingly
 VSDS_LDESConnectors=5886a1461f2565cee0e2bda8ef8650129ba32da8
-VSDS_LDES_E2E_testing=f5a60040b20d277b1b239b25d2052773c298bed3
+VSDS_LDES_E2E_testing=9c3c01ca4e1e8ecb97343e70892b70474f5ec549
 VSDS_LDESServer4J=0fc4b2073bbc1abc26ec025a4beb2eca80fdf36c
 VSDS_LDESWorkbench_NiFi=efe3ecbe736bd6d787845bbcb71f857447eb2dd5
 VSDS_LDESWorkbench_Services=2891957dad109793d91525bdcf60dd73c842bd7f
@@ -10,15 +10,15 @@ export GITHUB_TOKEN=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;
 
 
 #trivy the github repository with a specific commit
-echo "############trivy scan ../VSDS-LDESConnectors############"
+echo -e "\n\n############trivy scan ../VSDS-LDESConnectors############\n\n"
 trivy repo --offline-scan  --security-checks vuln,secret,config --severity HIGH,CRITICAL --commit ${VSDS_LDESConnectors:-4996d3b82eb2a241154bbf7547f4cfe04f114af0} -o ./RRepoCom/VSDS-LDESConnectors.output  https://github.com/Informatievlaanderen/VSDS-LDESConnectors;
-echo "############trivy scan ../VSDS-LDESServer4############"
+echo -e "\n\n############trivy scan ../VSDS-LDESServer4############\n\n"
 trivy repo --offline-scan  --security-checks vuln,secret,config --severity HIGH,CRITICAL --commit ${VSDS_LDESServer4J:-4996d3b82eb2a241154bbf7547f4cfe04f114af0} -o ./RRepoCom/VSDS-LDESServer4J.output  https://github.com/Informatievlaanderen/VSDS-LDESServer4J;
-echo "############trivy scan ../VSDS-LDESWorkbench-NiFi############" 
+echo -e "\n\n############trivy scan ../VSDS-LDESWorkbench-NiFi############\n\n" 
 trivy repo --offline-scan  --security-checks vuln,secret,config --severity HIGH,CRITICAL --commit ${VSDS_LDESWorkbench_NiFi:-4996d3b82eb2a241154bbf7547f4cfe04f114af0} -o ./RRepoCom//VSDS-LDESWorkbench-NiFi.output  https://github.com/Informatievlaanderen//VSDS-LDESWorkbench-NiFi;
-echo "############trivy scan ../VSDS-LDESWorkbench-Services############"
+echo -e "\n\n############trivy scan ../VSDS-LDESWorkbench-Services############\n\n"
 trivy repo --offline-scan  --security-checks vuln,secret,config --severity HIGH,CRITICAL --commit ${VSDS_LDESWorkbench_Services:-4996d3b82eb2a241154bbf7547f4cfe04f114af0} -o ./RRepoCom/VSDS-LDESWorkbench-Services.output  https://github.com/Informatievlaanderen/VSDS-LDESWorkbench-Services;
-echo "############trivy scan ../VSDS-LDES-E2E-testing############" 
+echo -e "\n\n############trivy scan ../VSDS-LDES-E2E-testing############\n\n" 
 trivy repo --offline-scan  --security-checks vuln,secret,config --severity HIGH,CRITICAL --commit ${VSDS_LDES_E2E_testing:-f5a60040b2052773c298bed3} -o ./RRepoCom/VSDS-LDES-E2E-testing.output  https://github.com/Informatievlaanderen/VSDS-LDES-E2E-testing;
 
 
