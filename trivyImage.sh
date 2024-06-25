eae24ab0fa3a2f63c5920a7f7411839176037b1f
XD <Xueying.Deng@imec.be>
2024-06-25 15:23:51 +0200

    feat: update to most recent repos.

--- trivyImage.sh
+++ trivyImage.sh
@@ -3,8 +3,9 @@
 
 #trivy the docker images
 echo -e "\n\n############trivy scan ldes-server############\n\n";
-trivy image --offline-scan --scanners vuln,secret,misconfig --severity HIGH,CRITICAL  ldes/ldes-server:2.15.0-SNAPSHOT > ./DockerImage/ldes-server.output;
-echo -e "\n\n############trivy scan ldi-workbench-nifi############\n\n";
-trivy image --offline-scan --scanners vuln,secret,misconfig --severity HIGH,CRITICAL  ldes/ldi-workbench-nifi:2.3.0-SNAPSHOT  >  ./DockerImage/ldi-workbench-nifi.output;
+trivy image --offline-scan --scanners vuln,secret,misconfig --severity HIGH,CRITICAL  ldes/ldes-server:3.1.0-SNAPSHOT > ./DockerImage/ldes-server.output;
+# echo -e "\n\n############trivy scan ldi-workbench-nifi############\n\n";
+# No build anymore, use Nexus instead
+# trivy image --offline-scan --scanners vuln,secret,misconfig --severity HIGH,CRITICAL  ldes/ldi-workbench-nifi:2.3.0-SNAPSHOT  >  ./DockerImage/ldi-workbench-nifi.output;
 echo "############trivy scan ldi-orchestrator############" ;
-trivy image --offline-scan --scanners vuln,secret,misconfig --severity HIGH,CRITICAL ldes/ldi-orchestrator:2.6.0-SNAPSHOT >  ./DockerImage/ldi-orchestrator.output;
+trivy image --offline-scan --scanners vuln,secret,misconfig --severity HIGH,CRITICAL ldes/ldi-orchestrator:2.7.0-SNAPSHOT >  ./DockerImage/ldi-orchestrator.output;
